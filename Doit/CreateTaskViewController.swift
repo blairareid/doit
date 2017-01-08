bc4a6fdaa68a9c0b686d39542aa3722abd87620d
Blair Reid <blair.reid@shaw.ca>
2017-01-08 08:14:47 -0800

    Add secondary controller

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Blair Reid on 2017-01-08.
+//  Copyright © 2017 Blair Reid. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+    
+    
+}
