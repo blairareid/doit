6385420f98f832318399b708d4a9963b503d665a
Blair Reid <blair.reid@shaw.ca>
2017-01-07 18:55:12 -0800

    stuff

---
+++ Doit/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Blair Reid on 2017-01-07.
+//  Copyright © 2017 Blair Reid. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var txtName = ""
+    var boolImportant = false
+}
--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -8,18 +8,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var tasks : [Task] = []  // Declaring array of Task objects
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()  // Load up the array of tasks
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Establishes the number of rows in the TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
 
+    
+    // Called on the setup of each row in the tableview
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.boolImportant {
+            cell.textLabel?.text = "! \(task.txtName)"
 
+        }
+            else {
+            cell.textLabel?.text = task.txtName
+           
+            }
+        
+        
+        return cell
+        
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.txtName = "Walk the dog"
+        task1.boolImportant = false
+
+        let task2 = Task()
+        task2.txtName = "Fire the gardner"
+        task2.boolImportant = true
+        
+        let task3 = Task()
+        task3.txtName = "Mow the lawn"
+        task3.boolImportant = false
+      
+        return ([task1, task2, task3])
+    }
+    
 }
 
